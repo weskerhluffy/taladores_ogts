58fd106b3562fe6be0aa230de1b85586ba374ae6
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2015-06-28 23:43:16 -0500

    eskeleto completo

--- common.mk
+++ common.mk
@@ -1,2 +1,4 @@
-ZLOG_DIR=/Users/ernesto/programas/zlog
 #ZLOG_DIR=/usr/local
+ZLOG_DIR=/Users/ernesto/programas/zlog
+#CACACOMUN_DIR=/home/ealvarad/workspace/unos_locos/src
+CACACOMUN_DIR=/Users/ernesto/workspace/unos_locos/src
---
+++ src/taladoresogts.c
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <cacacomun.h>
+#include "../src/taladoresogts.h"
