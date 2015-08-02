14a4d139d6997a5a106c3595e893ab1cf7d08f67
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2015-08-02 06:47:18 -0500

    terminadode codificar, faltan pruebas.

--- src/taladoresogts.c
+++ src/taladoresogts.c
@@ -2,3 +2,135 @@
 #include <stdio.h>
 #include <cacacomun.h>
 #include "../src/taladoresogts.h"
+
+/* Formula x = (b2 - b1)/(x1 - x2) */
+double taladores_ogts_determina_ordenada_interseccion(
+		tipo_dato indice_valor_dinamico_1, tipo_dato indice_valor_dinamico_2) {
+	double ordenada = 0;
+	double b2 = 0;
+	double b1 = 0;
+	double x2 = 0;
+	double x1 = 0;
+
+	b2 = *(chostos_minimos + indice_valor_dinamico_2);
+	b1 = *(chostos_minimos + indice_valor_dinamico_1);
+
+	x2 = *(costos_corte + indice_valor_dinamico_2);
+	x1 = *(costos_corte + indice_valor_dinamico_1);
+
+	ordenada = (b2 - b1) / (x1 - x2);
+
+	return ordenada;
+}
+
+void taladores_ogts_anade_linea(tipo_dato indice_linea) {
+	double intersexion_nueva = 0;
+
+	switch (pseudopila->total_lineas) {
+	case 0:
+		*(pseudopila->indices_lineas + pseudopila->total_lineas) = indice_linea;
+		pseudopila->total_lineas++;
+		break;
+	case 1:
+		intersexion_nueva = taladores_ogts_determina_ordenada_interseccion(
+				indice_linea,
+				*(pseudopila->indices_lineas + pseudopila->total_lineas - 1));
+		*(pseudopila->indices_lineas + pseudopila->total_lineas) = indice_linea;
+		pseudopila->total_lineas++;
+		pseudopila->total_intersexiones++;
+		break;
+	default:
+		while (pseudopila->total_lineas > 1) {
+			intersexion_nueva =
+					taladores_ogts_determina_ordenada_interseccion(indice_linea,
+							*(pseudopila->indices_lineas
+									+ pseudopila->total_lineas - 2));
+			if (intersexion_nueva
+					<= *(pseudopila->ordenadas_de_intersexiones
+							+ pseudopila->total_intersexiones - 1)) {
+
+				*(pseudopila->indices_lineas + pseudopila->total_lineas - 1) =
+						TALADORES_OGTS_VALOR_INVALIDO;
+				*(pseudopila->ordenadas_de_intersexiones
+						+ pseudopila->total_intersexiones - 1) =
+						TALADORES_OGTS_VALOR_INVALIDO;
+
+				pseudopila->total_lineas--;
+				pseudopila->total_intersexiones--;
+			} else {
+				// TODO: ESto sale del ciclo, o solo del switch?
+				break;
+			}
+
+		}
+
+		*(pseudopila->ordenadas_de_intersexiones
+				+ pseudopila->total_intersexiones) = intersexion_nueva;
+		*(pseudopila->indices_lineas + pseudopila->total_lineas) = indice_linea;
+
+		pseudopila->total_lineas++;
+		pseudopila->total_intersexiones++;
+
+		if (pseudopila->indice_caminado_intersexion
+				> pseudopila->total_intersexiones) {
+			pseudopila->indice_caminado_intersexion =
+					pseudopila->total_intersexiones;
+		}
+
+		break;
+	};
+}
+
+tipo_dato taladores_ogts_busqueda_minimo_en_lineas(tipo_dato ordenada) {
+	int idx_intersexion = 0;
+	double ordenada_pf = 0;
+	tipo_dato minimo = 0;
+
+	ordenada_pf = ordenada;
+
+	for (idx_intersexion = pseudopila->indice_caminado_intersexion;
+			idx_intersexion < pseudopila->total_intersexiones;
+			idx_intersexion++) {
+
+		if (ordenada_pf
+				< *(pseudopila->ordenadas_de_intersexiones + idx_intersexion)) {
+			break;
+		}
+
+	}
+
+	minimo = *(pseudopila->indices_lineas + idx_intersexion);
+	if (idx_intersexion == pseudopila->total_intersexiones) {
+		pseudopila->indice_caminado_intersexion = idx_intersexion - 1;
+	} else {
+		pseudopila->indice_caminado_intersexion = idx_intersexion;
+	}
+
+	return minimo;
+}
+
+tipo_dato taladores_ogts_busca_minimo(tipo_dato indice_linea,
+		tipo_dato ordenada) {
+	tipo_dato minimo = 0;
+
+	taladores_ogts_anade_linea(indice_linea);
+
+	minimo = taladores_ogts_busqueda_minimo_en_lineas(ordenada);
+
+	return minimo;
+}
+
+void taladores_ogts_encuentra_chosto_minimo() {
+	int i = 0;
+	tipo_dato dependiente_minima = 0;
+
+	*(chostos_minimos + 1) = 0;
+	*(chostos_minimos + 2) = *(costos_corte + 1) * *(alturas_arboles + 2);
+	taladores_ogts_anade_linea(2);
+
+	for (i = 3; i < num_arboles; i++) {
+		dependiente_minima = taladores_ogts_busca_minimo(i - 1, i);
+		*(chostos_minimos + i) = *(chostos_minimos + dependiente_minima)
+				+ *(alturas_arboles + i) * *(costos_corte + dependiente_minima);
+	}
+}
--- src/taladoresogts.h
+++ src/taladoresogts.h
@@ -14,6 +14,35 @@
 #define TALADORES_OGTS_MAX_FILAS_INPUT 3
 #define TALADORES_OGTS_MAX_COLS_INPUT 100000
 
+#define TALADORES_OGTS_VALOR_INVALIDO 10E12
+
+typedef struct taladores_ogts_pseudopila_lineas {
+	tipo_dato indice_caminado_intersexion;
+	tipo_dato total_intersexiones;
+	tipo_dato total_lineas;
+	double *ordenadas_de_intersexiones;
+	tipo_dato *indices_lineas;
+} taladores_ogts_pseudopila_lineas;
+
+tipo_dato num_arboles;
+tipo_dato *chostos_minimos;
+tipo_dato *costos_corte;
+tipo_dato *alturas_arboles;
+
+taladores_ogts_pseudopila_lineas *pseudopila;
+
+double taladores_ogts_determina_ordenada_interseccion(
+		tipo_dato indice_valor_dinamico_1, tipo_dato indice_valor_dinamico_2);
+
+void taladores_ogts_anade_linea(tipo_dato indice_linea);
+
+tipo_dato taladores_ogts_busqueda_minimo_en_lineas(tipo_dato ordenada);
+
+tipo_dato taladores_ogts_busca_minimo(tipo_dato indice_linea,
+		tipo_dato ordenada);
+
+void taladores_ogts_encuentra_chosto_minimo();
+
 double taladores_ogts_main();
 
 #endif /* TALADORESOGTS_H_ */
